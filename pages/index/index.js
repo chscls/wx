e77b64538a413932ca6539f900aae282fffda684
ChenHuan <chscls@163.com>
2018-05-28 17:40:19 +0800

    1111111

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,45 @@ Page({
   data: {
     motto: '陈芳颐',
     isRegister:true,
+    userInfo:{},
+    openid:''
+  },
+  getRegisterCode: function () {
+   console.log("xxxxxxxxx")
+  },
+  formSubmit: function (e) {
+    if (e.detail.value.realname== ""){
+      wx.showModal({
+        title: '表单错误',
+        content: '请输入真实姓名或者选择跳过，以后再输入',
+        showCancel: false
+      })
+      return
+    }
+    http.postReq("/wx/FyUserSvc/register",
+      {
+        ...e.detail.value,
+        openid: this.data.openid,
+        ...this.data.userInfo
+      
+      }, res => {
+
+          this.setData({ isRegister: true })
+      
+
+      })
+  },
+  formJump: function () {
+    http.postReq("/wx/FyUserSvc/register",
+      {
+        openid: this.data.openid,
+        ...this.data.userInfo
+      }, res => {
+
+        this.setData({ isRegister: true})
+        
+
+      })
   },
   print:function(text){
   console.log(text)
@@ -22,21 +61,30 @@ Page({
     })
   },
   onLoad: function () {
-    wx.login({
-      success: res => {
-        http.postReq("/wx/FyUserSvc/wxcode",
-          {
-            code: res.code
-          }, res => {
-           
-            if (res.id == null) {
-              this.setData({ isRegister: false })
-            }
-            
-          })
+    wx.getUserInfo({
+      success: res=> {
+        console.log(res.userInfo)
+        this.setData({"userInfo": res.userInfo})
+  
+        wx.login({
+          success: res => {
+            http.postReq("/wx/FyUserSvc/wxcode",
+              {
+                code: res.code
+              }, res => {
 
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                if (res.id == null) {
+                  this.setData({ isRegister: false, openid: res.openid  })
+                }
+
+              })
+
+            // /this.setData({ isRegister: false })
+            // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          }
+        })
       }
     })
+    
   }
 })
