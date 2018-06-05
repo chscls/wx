14bf3343d6381e355bdad0629bae89b367ac9f62
chscls@163.com <chscls@163.com>
2018-06-05 13:56:42 +0800

    1111111

--- pages/record/record.js
+++ pages/record/record.js
@@ -1,25 +1,36 @@
-var fileData = require('../../utils/data.js')
+var FyTestSvc = require('../../services/FyTestSvc')
 var util = require('../../utils/util')
+
 Page({
   // 页面初始数据
   data: {
     // nav 初始化
     // cas picker
+    sercherStorage: [],
+
+    inputValue: "",             //搜索框输入的值  
+
+    StorageFlag: true,         //显示搜索记录标志位
     casArray: ['美发', '美容', '美甲', '美睫'],
     casIndex: 0,
     // addr picker
-    addrArray: util.replacePhone(fileData.userData().addrs, false),
+    addrArray: util.replacePhone([], false),
     addrIndex: 0,
-    skillData: fileData.getSkilledData(),
+    skillData: [],
     curNavId: 1,
     curIndex: 0
   },
 
+
   onLoad: function () {
+
     var that = this
-    that.setData({
-      list: that.data.skillData
+    FyTestSvc.queryTest({ start: 0, count: 10 }, (data) => {
+      that.setData({
+        list: data
+      })
     })
+
   },
   // 跳转至详情页
   navigateDetail: function (e) {
@@ -33,9 +44,11 @@ Page({
     if (this.data.skillData.length === 0) return
     var that = this
     // 由于是模拟数据，加载更多时候，数据重复加载
-    that.data.skillData = that.data.skillData.concat(that.data.skillData)
-    that.setData({
-      list: that.data.skillData,
+    // that.data.skillData = that.data.skillData.concat(that.data.skillData)
+    FyTestSvc.queryTest({ start: 0, count: 10, userId: 1 }, (data) => {
+      that.setData({
+        list: data
+      })
     })
   },
   // 类别选择
@@ -51,7 +64,12 @@ Page({
     this.setData({
       addrIndex: e.detail.value
     })
-  }
+  },
+  wxSearchTab: function () {
 
+    wx.redirectTo({
+      url: '../record/search'
+    })
+  }
 
 })
