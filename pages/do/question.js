7d9ae0d95b13d4edb1dc1424c983badb1fd913b3
chscls@163.com <chscls@163.com>
2018-06-06 13:48:03 +0800

    1111111

--- pages/do/question.js
+++ pages/do/question.js
@@ -6,16 +6,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    testRecord:{}
+    testRecord:{},
+    current:1,
+    total:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+   var that = this;
     FyTestRecordSvc.addTestRecord(options, (data) => {
-      this.setData({ testRecord: data });
+     
+      this.setData({ testRecord: data ,total:data.questions.length});
+      wx.setNavigationBarTitle({
+        title: that.data.current + "/" + that.data.total 
+      })
     })
 
   },
