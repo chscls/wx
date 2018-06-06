74df6ce18623f21959a821b85b407cd6024bf737
chscls@163.com <chscls@163.com>
2018-06-06 13:12:55 +0800

    11111111111

---
+++ components/testItem/testItem.js
@@ -0,0 +1,25 @@
+// components/test/test.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    item: Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    navigateDetail: function (e) {
+      this.triggerEvent('navigateDetail', { item: this.data.item }) // 只会触发 pageEventListener2
+    }
+  }
+})
