0349b28d771cca44fbaf734361b3acfec8fef728
chscls@163.com <chscls@163.com>
2018-06-27 09:36:04 +0800

    11111111

--- components/news/lunbo.js
+++ components/news/lunbo.js
@@ -14,8 +14,7 @@ Component({
     movies: [
       { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
       { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
-      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
-      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' }
     ],
   },
 
