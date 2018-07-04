4d905f6e9ab439c47959d4f145e07674a4051adc
chscls@163.com <chscls@163.com>
2018-07-04 22:20:42 +0800

    11111

--- pages/do/question.js
+++ pages/do/question.js
@@ -33,7 +33,7 @@ Page({
           
           this.setData({
             testRecord: data,
-            btn: data.questions.length == 1 ? "提交" : "下一题",
+            btn: data.questions.length == 1 ? data.status == "create" ? "提交" : "返回": "下一题",
             answers: a
           });
         } else {
@@ -42,7 +42,7 @@ Page({
           wx.setStorageSync(data.uuid, b);
           this.setData({
             testRecord: data,
-            btn: data.questions.length==1?"提交":"下一题",
+            btn: data.questions.length == 1 ? data.status == "create" ? "提交" : "返回":"下一题",
             answers: b
           });
         }
@@ -50,7 +50,7 @@ Page({
         var b = data.answers
         this.setData({
           testRecord: data,
-          btn: data.questions.length == 1 ? "提交" : "下一题",
+          btn: data.questions.length == 1 ? data.status == "create" ? "提交" : "返回" : "下一题",
           answers: b
         });
       }
